98912d85d2a234fb1d2eaa4b13b12a3bd70cfed4
Guilherme Souza <guilhermesouzadossantos11@gmail.com>
2024-04-28 15:56:37 -0300

    Configurando header (falta responsividade) e adicionando novo background na tela principal (home)

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+var nav = document.querySelector('.navbar');
+console.log(nav)
+
+
+window.addEventListener('scroll', function(){
+    var scrollPosition = window.scrollY;
+    if(scrollPosition > 50){
+        nav.classList.add('nav-scroll-bg');
+    }else{
+        nav.classList.remove('nav-scroll-bg')
+    }
+})
